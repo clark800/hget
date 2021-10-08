0a6d514abf0897da95e4af0b0ffacdc7ab6a0e16
Chris Clark <clark800@users.noreply.github.com>
2021-10-07 20:03:48 -0700

    Show usage on flag argument

--- hget.c
+++ hget.c
@@ -239,7 +239,7 @@ static char* get_filename(char* path) {
 }
 
 int main(int argc, char **argv) {
-    if (argc != 2 && argc != 3)
+    if (argc < 2 || argc > 3 || argv[1][0] == '-')
         fail("usage: hget <url> [<dest>]", EUSAGE);
 
     // use fd 3 for progress updates if it has been opened
