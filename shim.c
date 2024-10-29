5fed45b106487663d57bd33a3d5b955ea0775a7a
Chris Clark <clark800@users.noreply.github.com>
2024-10-28 22:48:39 -0700

    Fix shim

--- shim.c
+++ shim.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include "shim.h"
 
+// we always call with the same cookie io functions so we can use a global
+static cookie_io_functions_t IO;
+
+static int reader(void* cookie, char* buf, int n) {
+    return (int)IO.read(cookie, buf, (size_t)n);
+}
+
+static int writer(void* cookie, const char* buf, int n) {
+    return (int)IO.write(cookie, buf, (size_t)n);
+}
+
 FILE* fopencookie(void* cookie, const char* mode, cookie_io_functions_t io) {
     (void)mode;
-    return funopen(cookie, (int (*)(void*, char*, int))io.read,
-            (int (*)(void*, const char*, int))io.write, NULL, io.close);
+    IO = io;
+    return funopen(cookie, reader, writer, NULL, io.close);
 }
